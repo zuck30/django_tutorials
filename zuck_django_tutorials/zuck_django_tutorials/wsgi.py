1e0bb04e82e1d739491776df4ede26bbc4c97de2
Timothy Zucker <155255765+zuck30@users.noreply.github.com>
2026-08-19 11:25:38 +0300

    Add a project

---
+++ zuck_django_tutorials/zuck_django_tutorials/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zuck_django_tutorials project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zuck_django_tutorials.settings")
+
+application = get_wsgi_application()
